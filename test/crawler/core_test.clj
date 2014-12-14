0d802beedf5e3c247dcf4fbe3fbdd88db9478304
Vadim Buko <VadBuka1993@gmail.com>
2014-12-14 13:32:57 +0300

    Add test and change readme

--- test/crawler/core_test.clj
+++ test/crawler/core_test.clj
@@ -2,6 +2,11 @@
   (:require [clojure.test :refer :all]
             [crawler.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-parse-urls
+  (let [body "<body><a href=\"https://google.com/page1.html\">link</a>
+    <h1>Header</h1><a href=\"http://ya.ru/page1.html\">link</a>
+    <br><a href=\"/about.html\">link</a>"]
+
+    (is (=
+      (parse-urls body)
+      '("https://google.com/page1.html" "http://ya.ru/page1.html")))))
