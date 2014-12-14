661430b1952ce993df068f18d0cd006d42c42b32
Vadim Buko <VadBuka1993@gmail.com>
2014-12-14 13:22:55 +0300

    Go parallel and check if link already visited

--- src/crawler/core.clj
+++ src/crawler/core.clj
@@ -11,27 +11,29 @@
   (map #(% 1) (re-seq #"href=[\'\"]?((http|https)\:\/\/[^\'\" >]+)" body))) ; "
 
 (defn visit-url
-  [url current-level max-level]
+  [url current-level max-level visited-urls]
   (cond
-    (<= current-level max-level)
-    (let [response @(http/get url {:follow-redirects false :throw-exceptions false})
-          status (:status response)
-          body (:body response)]
-      (cond
-        (= status 200)
-        (let [urls (parse-urls body)]
-          (print-level current-level url (count urls) "links")
-            (doall (pmap #(visit-url % (+ current-level 1) max-level) urls)))
+    (and (<= current-level max-level) (not (contains? @visited-urls url)))
+    (do
+      (swap! visited-urls #(conj % url))
+      (let [response @(http/get url {:follow-redirects false :throw-exceptions false})
+            status (:status response)
+            body (:body response)]
+        (cond
+          (= status 200)
+          (let [urls (parse-urls body)]
+            (print-level current-level url (count urls) "links")
+              (doall (pmap #(visit-url % (+ current-level 1) max-level visited-urls) urls)))
 
-        (contains? #{301 302 307} status)
-        (let [redirect-url (:location (:headers response))]
-          (print-level current-level url "redirect" redirect-url)
-          (visit-url redirect-url (+ current-level 1) max-level))
+          (contains? #{301 302 307} status)
+          (let [redirect-url (:location (:headers response))]
+            (print-level current-level url "redirect" redirect-url)
+            (visit-url redirect-url (+ current-level 1) max-level visited-urls))
 
-        :else
-        (print-level current-level url "bad")))))
+          :else
+          (print-level current-level url "bad"))))))
 
 (defn -main
   [url depth]
-  (visit-url url 1 (Integer/parseInt depth))
+  (visit-url url 1 (Integer/parseInt depth) (atom #{}))
   (shutdown-agents))
